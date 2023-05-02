4688af299e0b47ebbb9c325de2768ae42ad0799b
filipton <filipton12@gmail.com>
2023-05-02 18:00:22 +0200

    feat: working query

--- postgres-test/src/main.rs
+++ postgres-test/src/main.rs
@@ -1,11 +1,28 @@
-fn main() {
-    let login_query = generate_login("filipton", "9l2FcxtusEYC", "neondb");
-    let hex = hex::encode(&login_query);
-    println!("{}", hex);
+use websocket::{ClientBuilder, Message};
 
+#[tokio::main]
+async fn main() {
+    let mut ws_client =
+        ClientBuilder::new("wss://ep-steep-mud-147485.eu-central-1.aws.neon.tech/v2")
+            .unwrap()
+            .connect_secure(None)
+            .unwrap();
+
+    let login_query = generate_login("filipton", "9l2FcxtusEYC", "neondb");
     let generate_query = generate_query("SELECT now()");
-    let hex = hex::encode(&generate_query);
-    println!("{}", hex);
+
+    ws_client
+        .send_message(&Message::binary(login_query))
+        .unwrap();
+    for _ in 0..2 {
+        _ = ws_client.recv_message().unwrap();
+    }
+
+    ws_client
+        .send_message(&Message::binary(generate_query))
+        .unwrap();
+    let msg = ws_client.recv_message().unwrap();
+    println!("{:?}", msg);
 }
 
 const START_HEX: &'static [u8; 7] = &[0, 0, 0, 0x3C, 0, 0x03, 0];
@@ -25,13 +42,11 @@ fn generate_login(username: &str, password: &str, db: &str) -> Vec<u8> {
     tmp.extend_from_slice(UTF8_HEX);
     tmp.extend_from_slice(PASSWORD_HEX);
 
-    let password_be = (password.len() + 5).to_be_bytes();
-    let password_be = &password_be[(password_be.len() - 4)..];
+    let password_len_be: [u8; 4] = (password.len() as u32 + 5).to_be_bytes();
 
-    tmp.extend_from_slice(password_be);
+    tmp.extend_from_slice(&password_len_be);
     tmp.extend_from_slice(password.as_bytes());
     tmp.push(0);
-
     tmp
 }
 
@@ -40,5 +55,10 @@ fn generate_query(query: &str) -> Vec<u8> {
     let mut tmp = Vec::new();
     tmp.extend_from_slice(QUERY_HEX);
 
+    let query_len_be: [u8; 4] = (query.len() as u32 + 5).to_be_bytes();
+
+    tmp.extend_from_slice(&query_len_be);
+    tmp.extend_from_slice(query.as_bytes());
+    tmp.push(0);
     tmp
 }
