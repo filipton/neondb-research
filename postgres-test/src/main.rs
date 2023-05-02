2d5b7cee1fa79b94f35f41be48b610c071cfbf3c
filipton <filipton12@gmail.com>
2023-05-03 01:53:33 +0200

    feat: use of BufferWriter in `generate_query` and `generate_login` method

--- postgres-test/src/main.rs
+++ postgres-test/src/main.rs
@@ -10,36 +10,6 @@ mod parser;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut opts: Vec<(&str, &str)> = Vec::new();
-    opts.push(("user", "filipton"));
-    opts.push(("database", "neondb"));
-    opts.push(("client_encoding", "UTF8"));
-
-    let mut writer = buffer_writer::BufferWriter::new();
-    writer.write_i16(3).write_i16(0);
-
-    for (key, value) in opts {
-        writer.write_cstring(&key).write_cstring(&value);
-    }
-
-    let mut password_writer = buffer_writer::BufferWriter::new();
-    let password = password_writer
-        .write_cstring("9l2FcxtusEYC")
-        .flush(Some(b'p'));
-
-    println!("{:?}", hex::encode_upper(&password));
-
-    let length = writer.get_length();
-    let body = writer.write_cstring("").add(&password).flush(None);
-
-    let res = BufferWriter::new()
-        .write_i32(length as i32)
-        .add(&body)
-        .flush(None);
-
-    println!("{:?}", hex::encode_upper(res));
-    return Ok(());
-
     let mut ws_client =
         ClientBuilder::new("wss://ep-steep-mud-147485.eu-central-1.aws.neon.tech/v2")
             .unwrap()
@@ -48,7 +18,6 @@ async fn main() -> Result<()> {
 
     let login_query = generate_login("filipton", "9l2FcxtusEYC", "neondb");
     let generate_query = generate_query("SELECT * FROM tests");
-    println!("{:?}", hex::encode(&generate_query));
 
     ws_client
         .send_message(&Message::binary(login_query))
@@ -69,41 +38,35 @@ async fn main() -> Result<()> {
     Ok(())
 }
 
-// TODO: make serialzier for this LMAOOOO
-const START_HEX: &'static [u8; 7] = &[0, 0, 0, 0x3C, 0, 0x03, 0];
-const USER_HEX: &'static [u8; 6] = b"\x00user\x00";
-const DATABASE_HEX: &'static [u8; 10] = b"\x00database\x00";
-const CLIENT_ENCODING_HEX: &'static [u8; 17] = b"\x00client_encoding\x00";
-const UTF8_HEX: &'static [u8; 5] = b"UTF8\x00";
-const PASSWORD_HEX: &'static [u8; 2] = b"\x00p";
 fn generate_login(username: &str, password: &str, db: &str) -> Vec<u8> {
-    let mut tmp = Vec::new();
-    tmp.extend_from_slice(START_HEX);
-    tmp.extend_from_slice(USER_HEX);
-    tmp.extend_from_slice(username.as_bytes());
-    tmp.extend_from_slice(DATABASE_HEX);
-    tmp.extend_from_slice(db.as_bytes());
-    tmp.extend_from_slice(CLIENT_ENCODING_HEX);
-    tmp.extend_from_slice(UTF8_HEX);
-    tmp.extend_from_slice(PASSWORD_HEX);
+    let mut opts: Vec<(&str, &str)> = Vec::new();
+    opts.push(("user", username));
+    opts.push(("database", db));
+    opts.push(("client_encoding", "UTF8"));
 
-    let password_len_be: [u8; 4] = (password.len() as u32 + 5).to_be_bytes();
+    let mut writer = buffer_writer::BufferWriter::new();
+    writer.write_i16(3).write_i16(0);
 
-    tmp.extend_from_slice(&password_len_be);
-    tmp.extend_from_slice(password.as_bytes());
-    tmp.push(0);
-    tmp
+    for (key, value) in opts {
+        writer.write_cstring(&key).write_cstring(&value);
+    }
+
+    let mut password_writer = buffer_writer::BufferWriter::new();
+    let password = password_writer.write_cstring(password).flush(Some(b'p'));
+
+    let length = writer.get_length();
+    let body = writer.write_cstring("").add(&password).flush(None);
+
+    let res = BufferWriter::new()
+        .write_i32(length as i32)
+        .add(&body)
+        .flush(None);
+
+    res
 }
 
-const QUERY_HEX: &'static [u8; 1] = b"Q";
 fn generate_query(query: &str) -> Vec<u8> {
-    let mut tmp = Vec::new();
-    tmp.extend_from_slice(QUERY_HEX);
-
-    let query_len_be: [u8; 4] = (query.len() as u32 + 5).to_be_bytes();
-
-    tmp.extend_from_slice(&query_len_be);
-    tmp.extend_from_slice(query.as_bytes());
-    tmp.push(0);
-    tmp
+    let mut writer = buffer_writer::BufferWriter::new();
+    writer.write_cstring(query);
+    writer.flush(Some(b'Q'))
 }
